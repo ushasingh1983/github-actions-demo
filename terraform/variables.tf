c66b31814a66cf9b14bc649702b2410b6ba6ea00
Shailender Singh <50791698+shailenderbakshi@users.noreply.github.com>
2025-09-16 06:03:52 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,5 @@
+# terraform/variables.tf  (replace the whole file with this if easier)
+
 variable "location"        { type = string }
 variable "rg_name"         { type = string }
 variable "vnet_name"       { type = string }
@@ -5,7 +7,11 @@ variable "subnet_name"     { type = string }
 variable "vm_name"         { type = string }
 variable "vm_size"         { type = string }
 variable "admin_username"  { type = string }
-variable "admin_password"  { type = string sensitive = true }
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
 
 variable "create_public_ip" {
   description = "Attach a public IP (for quick RDP). Consider false for prod."
@@ -17,4 +23,3 @@ variable "rdp_allowed_cidr" {
   description = "CIDR allowed to RDP (3389). Use your IP/32."
   type        = string
 }
-
