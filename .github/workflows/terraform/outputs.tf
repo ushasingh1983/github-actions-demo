5b5a637e3dbb65cfbbc9199c74c43b2ca6e00703
Shailender Singh <50791698+shailenderbakshi@users.noreply.github.com>
2025-09-16 05:40:34 +0100

    Update outputs.tf

--- .github/workflows/terraform/outputs.tf
+++ .github/workflows/terraform/outputs.tf
@@ -1,1 +1,4 @@
+output "resource_group" { value = azurerm_resource_group.rg.name }
+output "vm_private_ip"  { value = azurerm_network_interface.nic.private_ip_address }
+output "vm_public_ip"   { value = try(azurerm_public_ip.pip[0].ip_address, null) }
 
